d5d60786f16aa3f9cd96ea0146e19701ca853c0c
FavorMylikes <l786112323@gmail.com>
2017-12-09 17:29:11 +0800

    生产环境和开发环境分离

--- bookborrow/settings.py
+++ bookborrow/settings.py
@@ -11,9 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-# 生产环境和开发环境分离
-from .conf.local import *
-from .conf.deploy import *
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -169,3 +167,8 @@ LOGGING = {
         },
     },
 }
+
+
+# 生产环境和开发环境分离
+from .conf.local import *
+from .conf.deploy import *
